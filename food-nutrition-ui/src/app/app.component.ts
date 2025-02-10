404f48ceffb54e80428094a0dea1e465c82b5940
jilbertvasquez <jilbertvasquez@gmail.com>
2025-02-10 21:12:21 +0800

    add header

--- food-nutrition-ui/src/app/app.component.ts
+++ food-nutrition-ui/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FoodNutritionContainerComponent } from './food-nutrition-container/food-nutrition-container.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [FoodNutritionContainerComponent],
+    imports: [FoodNutritionContainerComponent, HeaderComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
