5ec26693bf76fe7858a2d0be167eb9f149bb5fda
JilbertVasquez <jilbertvasquez@gmail.com>
2025-02-22 23:08:34 +0800

    clean header and app

--- food-nutrition-ui/src/app/app.component.ts
+++ food-nutrition-ui/src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
-import { FoodNutritionContainerComponent } from './food-nutrition-container/food-nutrition-container.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [FoodNutritionContainerComponent, HeaderComponent, RouterModule],
+    imports: [HeaderComponent, RouterModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
 export class AppComponent {
-    title = 'food-nutrition';
-
-
+    title = 'food-nutrition-app';
 }
