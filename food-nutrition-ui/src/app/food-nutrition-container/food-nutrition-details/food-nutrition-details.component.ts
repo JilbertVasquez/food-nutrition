7c835f8006ed8be060a4b39292e641bf66d99589
JilbertVasquez <jilbertvasquez@gmail.com>
2025-02-23 13:49:26 +0800

    clean food nutrition components

--- food-nutrition-ui/src/app/food-nutrition-container/food-nutrition-details/food-nutrition-details.component.ts
+++ food-nutrition-ui/src/app/food-nutrition-container/food-nutrition-details/food-nutrition-details.component.ts
@@ -1,13 +1,13 @@
-import { Component, computed, OnInit, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { FoodNutritionDetails } from '../dtos/food-description-dto';
+import { FoodNutritionDetails } from '../../dtos/food-description-dto';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
-import { NaturalNutrientsService } from '../_services/natural-nutrients.service';
-import { UserService } from '../_services/user.service';
-import { DonutChartComponent } from '../donut-chart/donut-chart.component';
-import { DialogService } from '../_services/dialog.service';
+import { NaturalNutrientsService } from '../../_services/natural-nutrients.service';
+import { UserService } from '../../_services/user.service';
+import { DonutChartComponent } from '../../donut-chart/donut-chart.component';
+import { DialogService } from '../../_services/dialog.service';
 import { lastValueFrom } from 'rxjs';
 
 @Component({
@@ -16,8 +16,7 @@ import { lastValueFrom } from 'rxjs';
     templateUrl: './food-nutrition-details.component.html',
     styleUrl: './food-nutrition-details.component.css'
 })
-export class FoodNutritionDetailsComponent implements OnInit {
-    // selectedFood: FoodNutritionDetails | null = null;
+export class FoodNutritionDetailsComponent {
 
     quantity = signal<number>(1);
 
@@ -29,30 +28,13 @@ export class FoodNutritionDetailsComponent implements OnInit {
         return this._computeData(selectedFood, this.quantity());
     })
 
-    // selectedFood = signal<FoodNutritionDetails>({
-    //     food_name: "bread",
-    //     serving_unit: "slice",
-    //     serving_qty: 1,
-    //     serving_weight_grams: 29,
-    //     nf_calories: 77.14,
-    //     nf_total_fat: 0.97,
-    //     nf_cholesterol: 0,
-    //     nf_sodium: 142.1,
-    //     nf_total_carbohydrate: 14.33,
-    //     nf_dietary_fiber: 0.78,
-    //     nf_sugars: 1.64,
-    //     nf_protein: 2.57,
-    //     nf_potassium: 36.54
-    // });
-
-    constructor(private _naturalNutrientsService: NaturalNutrientsService, private _userService: UserService, private _dialogService: DialogService) { }
-
-    ngOnInit() {
-
-    }
+    constructor(
+        private _naturalNutrientsService: NaturalNutrientsService,
+        private _userService: UserService,
+        private _dialogService: DialogService
+    ) { }
 
     increaseQuantity() {
-        console.log(this.quantity());
         this.quantity.update(q => q + 1);
     }
 
@@ -95,11 +77,9 @@ export class FoodNutritionDetailsComponent implements OnInit {
 
         const userFoodIntake = this._userService.userFoodIntake();
 
-        // Find existing food item in the array
         const existingFood = userFoodIntake.find(food => food.food_name === selectedFood.food_name);
 
         if (existingFood) {
-            // Append all nutritional values instead of overwriting
             existingFood.serving_qty += selectedFood.serving_qty;
             existingFood.serving_weight_grams += selectedFood.serving_weight_grams;
             existingFood.nf_calories += selectedFood.nf_calories;
@@ -112,12 +92,7 @@ export class FoodNutritionDetailsComponent implements OnInit {
             existingFood.nf_protein += selectedFood.nf_protein;
             existingFood.nf_potassium += selectedFood.nf_potassium;
         } else {
-            // Push new food item if it doesn't exist
-            userFoodIntake.push({ ...selectedFood }); // Using spread operator to prevent reference issues
+            userFoodIntake.push({ ...selectedFood });
         }
-
-        console.log(this._userService.userFoodIntake());
     }
-
-
 }
--- food-nutrition-ui/src/app/user/users-food-intake-list/users-food-intake-list.component.ts
+++ food-nutrition-ui/src/app/user/users-food-intake-list/users-food-intake-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Signal } from '@angular/core';
+import { Component, Signal } from '@angular/core';
 import { FoodNutritionDetails } from '../../dtos/food-description-dto';
 import { UserService } from '../../_services/user.service';
 import { MatCardModule } from '@angular/material/card';
@@ -16,25 +16,12 @@ import { MatButtonModule } from '@angular/material/button';
 export class UsersFoodIntakeListComponent {
     userFoodIntake: Signal<FoodNutritionDetails[]>;
 
-    userFoodList = computed(() => {
-        const foodList = this.userFoodIntake();
-
-        if (!foodList) return [];
-        return foodList;
-        // return foodList.map((food) => ({
-        //     food_name: food.food_name,
-        //     serving_qty: food.serving_qty,
-        //     serving_unit: food.serving_unit
-        // }));
-    })
-
     constructor(private _userService: UserService, private _dialogService: DialogService) {
         this.userFoodIntake = this._userService.userFoodIntake.asReadonly();
     }
 
     async openFoodDetails(foodData: FoodNutritionDetails) {
         const newData = await lastValueFrom(this._dialogService.foodNutritionDetailsModal(foodData).afterClosed());
-        console.log("NEW DATA:", newData);
 
         if (newData) {
             this._userService.updateUserFoodIntake(newData);
