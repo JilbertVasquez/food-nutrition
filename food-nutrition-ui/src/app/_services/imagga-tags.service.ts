2d05c39c616698a9ff7f78c857f234b0133a9ec6
JilbertVasquez <jilbertvasquez@gmail.com>
2025-02-23 11:38:54 +0800

    clean imagga part (image tagger)

--- food-nutrition-ui/src/app/_services/imagga-tags.service.ts
+++ food-nutrition-ui/src/app/_services/imagga-tags.service.ts
@@ -22,17 +22,6 @@ export class ImaggaTagsService {
         this.imageSrc.set(image);
     }
 
-    // getImaggaImageTags(image_url: string = 'https://www.jocooks.com/wp-content/uploads/2020/03/white-bread-1.jpg') {
-    //     const headers = new HttpHeaders({
-    //         'Authorization': this._imaggaToken,
-    //     });
-
-    //     const params = { image_url };
-
-    //     const url = `${this._baseUrl}`;
-    //     return lastValueFrom(this.http.get<any>(url, { headers, params }));
-    // }
-
     getImaggaImageTags(image: string | ArrayBuffer) {
         const headers = new HttpHeaders({
             'Authorization': this._imaggaToken,
@@ -41,10 +30,6 @@ export class ImaggaTagsService {
         const formData = new FormData();
         formData.append('image_base64', image.toString().split(',')[1]);
 
-        const params = { image_url: formData };
-
-        const url = `${this._baseUrl}`;
-        // return lastValueFrom(this.http.get<any>(url, { headers, params }));
-        return lastValueFrom(this.http.post<ImaggaResponse>(url, formData, { headers }));
+        return lastValueFrom(this.http.post<ImaggaResponse>(this._baseUrl, formData, { headers }));
     }
 }
