968c33bb4e2df027f677e9c5e8a6767473ca32cb
JilbertVasquez <jilbertvasquez@gmail.com>
2025-02-26 21:58:25 +0800

    delete old tokens

--- food-nutrition-ui/src/environments/environment.development.ts
+++ food-nutrition-ui/src/environments/environment.development.ts
@@ -6,8 +6,4 @@ export const environment = {
 
     imaggaBaseUrl: "https://api.imagga.com/v2/tags",
     imaggaToken: "Basic YWNjXzZlOTI1MjE1OTI5NDYyNTpiNGJhZGY4M2UxYTRhMDI0YWE2NGZjY2EzNDRkYTE3MQ=="
-
-    // nutritionixAppId: "96b23a6b",
-    // nutritionixAppKey: "2dd0d2e4b78d912dbf272377a2719ea2",
-    // imaggaToken: "Basic YWNjXzc2NTMyYjA5ZjRmMjAxMDoxY2EzMzBjY2I3MmNlMjE1MmM0OTBhZjI5NGUxMDJjMw=="
 };
